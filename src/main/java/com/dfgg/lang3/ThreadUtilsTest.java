8dd7bd4c533bc026a91e28e85cd8959a0bd5a939
83531 <83531@DFGG-THINKPAD>
2018-06-06 09:09:35 +0800

    webapp replace webcontent

--- src/main/java/com/dfgg/lang3/ThreadUtilsTest.java
+++ src/main/java/com/dfgg/lang3/ThreadUtilsTest.java
@@ -5,7 +5,7 @@ public class ThreadUtilsTest {
 
 	public static void main(String [] args) {
 		int[] arr = new int[10];
-		int[] arr2 = new int[10];
+//		int[] arr2 = new int[10];
 		
 		arr[1] = 20;
 		
